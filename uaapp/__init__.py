26a0b379c045f1e78997158cd488f90d4c1396e4
Chris Nelson <cnelson@cnelson.org>
2016-04-28 10:43:32 -0700

    fix import

--- uaapp/__init__.py
+++ uaapp/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-import uaapp.views  # NOQA
-
 app = Flask(__name__)
 app.config.from_object('uaapp.default_settings')
+
+import uaapp.views  # NOQA
 
 try:
     app.config.from_envvar('UAAPP_SETTINGS')
